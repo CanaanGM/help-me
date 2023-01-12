a544d76f1518b05ea90aaff280cb79afb5c2a405
CanaanGM <reakak1@gmail.com>
2023-01-12 07:53:24 +0300

    for mat ing

--- help_me.py
+++ help_me.py
@@ -4,11 +4,11 @@ from urllib.error import HTTPError
 from dotenv import dotenv_values
 import requests
 
-APIKEY: str =  dotenv_values("./.env").get("APIKEY", None)
+APIKEY: str = dotenv_values("./.env").get("APIKEY", None)
 APIURL: str = "https://api.openai.com/v1/completions"
 
 
-def ask_chat(question: str) -> List[str] :
+def ask_chat(question: str) -> List[str]:
     """Contacts ChatGPT with the provided question and returns an answer
 
     Args:
@@ -18,35 +18,29 @@ def ask_chat(question: str) -> List[str] :
         List[str]: the answer list gotten from the space wizard
     """
 
-    chat_req_headers = {
-        "Authorization": f"Bearer {APIKEY}"
-    }
+    chat_req_headers = {"Authorization": f"Bearer {APIKEY}"}
 
     chat_req_body = {
         "model": "text-davinci-003",
         "prompt": question,
         "temperature": 1,
-        "max_tokens": 100
+        "max_tokens": 100,
     }
     try:
 
         request = requests.post(APIURL, headers=chat_req_headers, json=chat_req_body)
         request.raise_for_status()
 
-
         res = request.json()
         return res.get("choices", None)
     except HTTPError as ex:
-        print(
-            "something went wrong contacting the space wizard. . .",
-            ex,
-            end="\n"
-            )
+        print("something went wrong contacting the space wizard. . .", ex, end="\n")
         #  don't return just goooOooOo.
         #  is this bad design ? ? ?
         sys.exit(1)
 
-def extract_answer(answeres: List[str]) -> str :
+
+def extract_answer(answeres: List[str]) -> str:
     if answeres is None:
         return "No answers provided"
 
