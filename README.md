4317228e220b4f162409c46737082063c331322d
CanaanGM <reakak1@gmail.com>
2023-01-12 07:52:04 +0300

    in python. . . 'python sounds!!'

--- README.md
+++ README.md
@@ -5,11 +5,14 @@ reference code and idea by [Les Jackson](https://www.youtube.com/watch?v=25i-Dh6
 ### Setup
 for now to run this correctly u need to add your api-key as a dotnet secret -[Docs](https://learn.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-7.0&tabs=windows)-, will think of a better way later.
 [GET YOUR APIKEY](https://beta.openai.com/account/api-keys)
+#### c#
 
 ```powershell
 dotnet user-secrets init
 dotnet user-secrets set "APIKEY" "YOUR KEY"
 ```
+#### python
+- create a `.env` file and add into it: `APIKEY`
 ### usage:
 
 ```powershell
@@ -21,5 +24,6 @@ help-me "powershell forlop syntax"
 TODO:
 - [ ] make this a bona fide CLI app ; make it look and behave like one !
 - [ ] get a user API key, save it in a .config file in the same directory, or as an enviornment variable then try to get that . . .
-- [ ] make a rust\python implementation.
+- [x] make a python implementation.
+- [ ] make a rust implementation.
 - [ ] maybe some tests 
---
+++ help_me.py
@@ -0,0 +1,64 @@
+import sys
+from typing import List
+from urllib.error import HTTPError
+from dotenv import dotenv_values
+import requests
+
+APIKEY: str =  dotenv_values("./.env").get("APIKEY", None)
+APIURL: str = "https://api.openai.com/v1/completions"
+
+
+def ask_chat(question: str) -> List[str] :
+    """Contacts ChatGPT with the provided question and returns an answer
+
+    Args:
+        question (str): thy's query
+
+    Returns:
+        List[str]: the answer list gotten from the space wizard
+    """
+
+    chat_req_headers = {
+        "Authorization": f"Bearer {APIKEY}"
+    }
+
+    chat_req_body = {
+        "model": "text-davinci-003",
+        "prompt": question,
+        "temperature": 1,
+        "max_tokens": 100
+    }
+    try:
+
+        request = requests.post(APIURL, headers=chat_req_headers, json=chat_req_body)
+        request.raise_for_status()
+
+
+        res = request.json()
+        return res.get("choices", None)
+    except HTTPError as ex:
+        print(
+            "something went wrong contacting the space wizard. . .",
+            ex,
+            end="\n"
+            )
+        #  don't return just goooOooOo.
+        #  is this bad design ? ? ?
+        sys.exit(1)
+
+def extract_answer(answeres: List[str]) -> str :
+    if answeres is None:
+        return "No answers provided"
+
+    return answeres[0].get("text", "there were no answeres")
+
+
+def main(question: str) -> None:
+    print(extract_answer(ask_chat(question)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or sys.argv[1] == " ":
+        print("Usage: help-me <Query>")
+        sys.exit(1)
+    main(sys.argv[1])
